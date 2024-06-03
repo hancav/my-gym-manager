d6f7331449f7319f03ed8462c74fc4da55f54cb9
Hugo Cavalaria <hc@mail.pt>
2024-06-03 18:09:50 +0100

    refactor multiple new routes for 3 types of users

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,16 @@
 <?php
+
+use App\Http\Controllers\BookingController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Admin\DashboardController;
+//use App\Http\Controllers\Admin\DashboardController;
 # Livewire
 use App\Livewire\Counter;
 use App\Http\Controllers\Inference\InferenceController;
+#
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ScheduledClassController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -19,9 +25,41 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/counter', Counter::class);
+//Route::get('/counter', Counter::class);
+//Route::get('/dashboard', [DashboardController::class, 'index'])->middleware(['auth'])->name('dashboard'); 
 
-Route::get('/dashboard', [DashboardController::class, 'index'])->middleware(['auth'])->name('dashboard'); 
+Route::get('/dashboard',DashboardController::class)->middleware(['auth'])->name('dashboard');
+
+Route::get('/admin/dashboard',function () {
+    return view('admin.dashboard');
+})->middleware(['auth','role:admin'])->name('admin.dashboard'); 
+
+Route::get('/instructor/dashboard',function () {
+    return view('instructor.dashboard');
+})->middleware(['auth','role:instructor'])->name('instructor.dashboard'); 
+
+Route::resource('instructor/schedule',ScheduledClassController::class )
+->only(['index','create','store','destroy'])
+->middleware(['auth','role:instructor']);
+
+// Route::get('/member/dashboard',function () {
+//     return view('member.dashboard');
+// })->middleware(['auth','role:member'])->name('member.dashboard'); 
+
+// Route::get('/member/book',[BookingController::class,'create'])->middleware(['auth','role:member'])->name('booking.create');
+
+// Member routes
+Route::middleware(['auth','role:member'])->group(function () {
+
+    Route::get('/member/dashboard',function () {
+        return view('member.dashboard');
+    })->name('member.dashboard'); 
+    
+    Route::get('/member/book',[BookingController::class,'create'])->name('booking.create');
+    Route::post('/member/bookings',[BookingController::class,'store'])->name('booking.store');
+    Route::get('/member/bookings',[BookingController::class,'index'])->name('booking.index');
+    Route::delete('/member/bookings/',[BookingController::class,'destroy'])->name('booking.destroy');
+});
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
