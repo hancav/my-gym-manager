6ddad4c8e47aa564012d90ea7d98329b2af997a3
Hugo Cavalaria <hc@mail.pt>
2024-06-03 18:41:40 +0100

    add created seeders to database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,11 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Article;
+use App\Models\ClassType;
+// use user seeder
+use Database\Seeders\UserSeeder;
+use Database\Seeders\ClassTypeSeeder;
+use Database\Seeders\ScheduledClassSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,9 +18,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        Article::factory()->times(50)->create();
-        // \App\Models\User::factory(10)->create();
+        $this->call([
+            UserSeeder::class,
+            ClassTypeSeeder::class,
+            ScheduledClassSeeder::class,
+        ]); 
 
+        // Article::factory()->times(50)->create();
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
