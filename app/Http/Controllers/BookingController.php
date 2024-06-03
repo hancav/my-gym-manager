ca4fab6ee8811c370df9d6f0526a1209cb0d7010
Hugo Cavalaria <hc@mail.pt>
2024-06-03 18:12:50 +0100

    Create a new booking controller

---
+++ app/Http/Controllers/BookingController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\ScheduledClass;
+
+class BookingController extends Controller
+{
+    //
+    public function create()
+    {
+        $scheduledClasses = ScheduledClass::where('date_time','>',now())->oldest()->get();
+
+        return view('member.book')->with('scheduledClasses',$scheduledClasses);
+    }
+}
