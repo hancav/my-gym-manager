484951f8ab4c52580c43f9af1c1f6a50193b0e3e
Hugo Cavalaria <hc@mail.pt>
2024-06-03 19:44:44 +0100

    booking controller now with scopes and new methods: store, index and destroy

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -11,11 +11,32 @@ class BookingController extends Controller
     //
     public function create()
     {
-        $scheduledClasses = ScheduledClass::where('date_time','>',now())
+        $scheduledClasses = ScheduledClass::upcoming()
             ->with('classType','instructor')
-            ->oldest()
+            ->notBooked()
+            ->oldest('date_time')
             ->get();
 
         return view('member.book')->with('scheduledClasses',$scheduledClasses);
     }
+
+    public function store(Request $request)
+    {
+       auth()->user()->bookings()->attach($request->scheduled_class_id);
+       return redirect()->route('booking.index');
+    }
+
+    public function index()
+    {
+        $bookings = auth()->user()->bookings()->upcoming()->get();
+
+        return view('member.upcoming')->with('bookings',$bookings);
+    }
+
+    public function destroy(int $id)
+    {
+        auth()->user()->bookings()->detach($id);
+        return redirect()->route('booking.index');
+    }  
+
 }
