115d7e8859565485167266fced6b77859bf3f23d
Hugo Cavalaria <hc@mail.pt>
2024-06-04 17:26:34 +0100

    create new mailable class to send class canceled to members

---
+++ resources/views/emails/class-canceled.blade.php
@@ -0,0 +1,3 @@
+<p> Hi </p>
+<p> Sorry your {{ $details['className'] }} class on {{ $details['classDateTime']->format('jS F') }} at {{ $details['classDateTime']->format('g:i a') }}
+    was canceled by the instructor. Check the schedule and book another class. Thank you </p>
